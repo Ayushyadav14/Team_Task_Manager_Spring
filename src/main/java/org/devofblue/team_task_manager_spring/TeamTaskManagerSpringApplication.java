63d4cd255872f6dce44d6bf1bfb05eaf14e37df0
Ayush Yadav <ayushyadav14012002@gmail.com>
2026-05-20 14:33:00 +0530

    project initialization

---
+++ src/main/java/org/devofblue/team_task_manager_spring/TeamTaskManagerSpringApplication.java
@@ -0,0 +1,13 @@
+package org.devofblue.team_task_manager_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamTaskManagerSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamTaskManagerSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/devofblue/team_task_manager_spring/TeamTaskManagerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devofblue.team_task_manager_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamTaskManagerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
